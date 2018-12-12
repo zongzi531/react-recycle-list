374841c066e38d4583c957a2afef2a7f4927a7f5
zongzi531 <zongzi.xy@gmail.com>
2018-12-12 19:18:06 +0800

    update RecycleList

--- src/components/RecycleList.tsx
+++ src/components/RecycleList.tsx
@@ -7,62 +7,96 @@ interface IRLProps {
 }
 
 interface IRLState {
+  childrenCount: number
+  refVirtualWrapper: React.RefObject<HTMLDivElement>
   refWrapper: React.RefObject<HTMLDivElement>
+  startChildren: number
+  topRange: number
   virtualHeight: number
+  virtualwrapperTop: number
+  wrapperHeight: number
 }
 
 export default class RecycleList extends React.Component<IRLProps, IRLState> {
   constructor(props: IRLProps) {
     super(props);
     this.state = {
+      childrenCount: 0,
+      refVirtualWrapper: React.createRef(),
       refWrapper: React.createRef(),
+      startChildren: 0,
+      topRange: 0,
       virtualHeight: 0,
+      virtualwrapperTop: 0,
+      wrapperHeight: 0,
     };
   }
 
   public handleScroll = () => {
-    const current = this.state.refWrapper.current as HTMLDivElement;
-    // this.setVirtualHeight()
-    // const { top: currentTop } = current.getBoundingClientRect();
-    // tslint:disable-next-line:no-console
-    console.log(current.getBoundingClientRect())
-    // tslint:disable-next-line:no-console
-    console.log(this.state.virtualHeight)
+    // get wrapper wrapper ref current property
+    const virtualCurrent = this.state.refVirtualWrapper.current as HTMLDivElement;
+    // current could be null
+    if (virtualCurrent) {
+      // get top, wrapper height, children height
+      const { top } = virtualCurrent.getBoundingClientRect()
+      const { wrapperHeight, topRange } = this.state
+      const { childrenHeight } = this.props
+      const virtualwrapperTop = top - topRange
+      // calculating need show children nodes in virtual wrapper
+      const childrenCount = Math.ceil(wrapperHeight / childrenHeight)
+      const startChildren = Math.floor(Math.abs(virtualwrapperTop) / childrenHeight)
+      this.setState({
+        childrenCount,
+        startChildren,
+        virtualwrapperTop,
+      })
+    }
   }
 
-  public setVirtualHeight () {
-    // I can't use this function, because I can't get all children rendered DOM height.
-    // So, i defined props.childrenHeight to get all children DOM height.
-    // Now, i need currentDate as small as possible and currentDate translate attribute can following.
+  public init () {
     const current = this.state.refWrapper.current as HTMLDivElement;
-    this.setState({
-      virtualHeight: Array.from(current.children).reduce((total, currentValue) => total += currentValue.clientHeight, 0)
-    })
+    const virtualCurrent = this.state.refVirtualWrapper.current as HTMLDivElement;
+    if (current && virtualCurrent) {
+      const { top: topRange } = virtualCurrent.getBoundingClientRect()
+      const { height } = current.getBoundingClientRect()
+      const { childrenHeight, data } = this.props
+      this.setState({
+        topRange,
+        virtualHeight: childrenHeight * data.length,
+        wrapperHeight: height,
+      }, () => {
+        this.handleScroll()
+      })
+    }
   }
 
   public get currentDate () {
-    return this.props.data.slice(10,60)
+    return this.props.data.slice(this.state.startChildren, this.state.startChildren + this.state.childrenCount)
+  }
+
+  public get translateY () {
+    const { childrenHeight } = this.props;
+    const translateY = Math.abs(this.state.virtualwrapperTop) - Math.abs(this.state.virtualwrapperTop) % childrenHeight;
+    return `translateY(${translateY}px)`;
   }
 
   public componentDidMount () {
-    this.setState({
-      virtualHeight: this.props.childrenHeight * this.props.data.length
-    })
-    window.addEventListener('scroll', this.handleScroll, true);
-  }
-  public componentWillUnmount () {
-    window.removeEventListener('scroll', this.handleScroll, true);
+    this.init()
   }
 
   public render() {
     return (
-      <div className="wrapper" ref={this.state.refWrapper}>
-        <div className="virtual-wrapper" style={{
+      <div className="wrapper" ref={this.state.refWrapper} onScroll={this.handleScroll}>
+        <div className="virtual-wrapper" ref={this.state.refVirtualWrapper} style={{
           height: this.state.virtualHeight
         }}>
           {
             this.currentDate.map((value, index) => {
-              return (<div key={index}>{value}</div>)
+              return (<div key={index} style={{
+                fontSize: '14px',
+                lineHeight: '19px',
+                transform: this.translateY,
+              }}>{value} [top:{this.state.virtualwrapperTop}]</div>)
             })
           }
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import RecycleList from './components/RecycleList';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const data = new Array(100).fill('node').map((value, index) => value + (index + 1))
+const data = new Array(100000).fill('node').map((value, index) => `${value} (${index + 1})`);
 
 ReactDOM.render(
   <React.Fragment>
-    <RecycleList data={data} childrenHeight={21} />
+    <h1 className="title">Recycle List</h1>
+    <div className="content">
+      <RecycleList data={data} childrenHeight={19} />
+    </div>
   </React.Fragment>,
   document.getElementById('root') as HTMLElement
 );
